204bb2983ccedd30038298bfeff7951a177d20d4
Mark65537 <Mark65537@ETS-PLAZMA-PC04>
2024-04-26 17:28:26 +0700

    new: добавленна функция ConvImgToSegaStr

--- RPConvert/Palette.cs
+++ RPConvert/Palette.cs
@@ -14,6 +14,32 @@ namespace RIConvert
 {
     public class Palette
     {
+        public static void ConvImgToSegaStr (Bitmap bmp, string savePath)
+        {
+            HashSet<Color> bmpPalette = GetPalette(bmp);
+
+            List<int> palette9bit = ConvertColorsTo9bit(bmpPalette);
+
+            List<string> hexPalette = palette9bit.Select(x => string.Format("${0:X3}", x)).ToList();
+
+            string hexPaletteString = string.Join(", ", hexPalette);
+        }
+
+        private static int ConvertColorTo9bit(Color color)
+        {
+            return ((color.B >> 5) << 9) |
+                        ((color.G >> 5) << 5) |
+                            ((color.R >> 5) << 1);
+        }
+        public static List<int> ConvertColorsTo9bit(HashSet<Color> palette)
+        {
+            List<int> palette9bit = new();
+            foreach (var color in palette)
+            {
+                palette9bit.Add(ConvertColorTo9bit(color));
+            }
+            return palette9bit;
+        }
         public static Bitmap ConvertToXColors(Bitmap originalImage, int colorCount, HashSet<Color> customPal = null)
         {
 
